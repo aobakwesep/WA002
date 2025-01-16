c8dd68a5b4f8b6d938b9cf9149a62a8d2419c44e
aobakwesep <aobakwesep@gmail.com>
2025-01-16 22:21:11 +0200

    Updated index.js
    Added elements to innerHTML

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 function showTemp(response) {
 let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
 let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
+
+console.log(response.data);
 
 cityElement.innerHTML = response.data.city;
-temperatureElement.innerHTML = Math.round(response.data.temperature.current); 
+descriptionElement.innerHTML = response.data.condition.description;
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+temperatureElement.innerHTML = Math.round(temperature); 
 
 }
 
