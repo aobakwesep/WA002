2c87a9e69ba068442e3ad2eddd35d33f65b23f0c
aobakwesep <aobakwesep@gmail.com>
2025-01-17 11:47:12 +0200

    Updated index.js
    Added a function to format the date and time

--- src/index.js
+++ src/index.js
@@ -5,16 +5,32 @@ let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windSpeedElement = document.querySelector("#wind-speed");
-
-console.log(response.data);
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
 
 cityElement.innerHTML = response.data.city;
+timeElement.innerHTML = formatDate(date);
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
 temperatureElement.innerHTML = Math.round(temperature); 
 
 }
+
+function formatDate(date) {
+ 
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 
 function searchCity(city) {
 let apiKey = "5c8e18035abacoef369tb23f0434da2c";
